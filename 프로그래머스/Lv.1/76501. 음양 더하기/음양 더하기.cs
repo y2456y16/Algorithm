237441736be9cca96e6243cd4e1027657efad81b
y2456y16 <y2456y16@gmail.com>
2024-01-16 09:41:02 +0900

    [level Lv.1] Title: 음양 더하기, Time: 0.28 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/76501. 음양 더하기/음양 더하기.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int[] absolutes, bool[] signs) {
+        int answer = 123456789;
+        answer =0;
+        
+        for(int i=0; i<absolutes.Length; i++)
+        {
+            if(signs[i] == true)
+                answer += absolutes[i];
+            else if(signs[i] == false)
+                answer -= absolutes[i];
+        }
+        return answer;
+    }
+}
