8e2cf7427b430f17747adcc273844270be375650
y2456y16 <y2456y16@gmail.com>
2024-01-18 09:02:43 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.18 ms, Memory: 31 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.cs
@@ -0,0 +1,19 @@
+public class Solution {
+    public string solution(int n) {
+        string answer = "";
+        
+        for(int i=1; i<=n; i++)
+        {
+            if(i%2==1)
+            {
+                answer += "수";
+            }
+            else
+            {
+                answer += "박";
+            }
+        }
+        
+        return answer;
+    }
+}
