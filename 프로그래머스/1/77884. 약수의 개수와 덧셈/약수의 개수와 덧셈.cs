ff771381be4a7d5c02de5bd1c667646c13d91448
y2456y16 <y2456y16@gmail.com>
2024-01-19 09:04:36 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 0.21 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.cs
@@ -0,0 +1,32 @@
+using System;
+
+public class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+        int count =0;
+        
+        
+        for(int i=left; i<=right; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                if(i%j==0)
+                {
+                    count++;
+                }
+            }
+            
+            if(count%2==0)
+            {
+                answer += i;
+                count =0;
+            }
+            else
+            {
+                answer -= i;
+                count =0;
+            }
+        }
+        return answer;
+    }
+}
