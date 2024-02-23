33638dc5e42c55b68d7110fde4be5eca7f8c3eaf
y2456y16 <y2456y16@gmail.com>
2024-02-23 09:43:25 +0900

    [level 1] Title: 공원 산책, Time: 3.72 ms, Memory: 31.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.cs
@@ -0,0 +1,81 @@
+using System;
+
+public class Solution
+{
+    public int[] solution(string[] park, string[] routes)
+    {
+        int maxX = park[0].Length;
+        int maxY = park.Length;
+
+        int[] answer = new int[2];
+
+        int x = 0;
+        int y = 0;
+
+        for (int i = 0; i < park.Length; i++)
+        {
+            int index = park[i].IndexOf("S");
+            if (index != -1)
+            {
+                x = index;
+                y = i;
+                break;
+            }
+        }
+
+        foreach (string route in routes)
+        {
+            string[] routeInfo = route.Split(" ");
+
+            string dir = routeInfo[0];
+            int movePos = int.Parse(routeInfo[1]);
+
+            int nextY = y;
+            int nextX = x;
+            bool isCheck = true;
+
+            for (int i = 0; i < movePos; i++)
+            {
+                switch (dir)
+                {
+                    case "N":
+                        nextY--;
+                        break;
+                    case "S":
+                        nextY++;
+                        break;
+                    case "W":
+                        nextX--;
+                        break;
+                    case "E":
+                        nextX++;
+                        break;
+                }
+
+                if (IsOderCheck(park, nextY, nextX, maxX, maxY) == false)
+                {
+                    isCheck = false;
+                    break;
+                }
+            }
+            if (isCheck)
+            {
+                x = nextX;
+                y = nextY;
+            }
+        }
+        return new int[2] { y, x };
+    }
+
+    bool IsOderCheck(string[] park, int y, int x, int maxX, int maxY)
+    {
+        if (y < 0 || y >= maxY)
+            return false;
+        if (x < 0 || x >= maxX)
+            return false;
+        if (park[y][x] == 'X')
+            return false;
+
+        return true;
+    }
+}
